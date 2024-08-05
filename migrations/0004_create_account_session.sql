9b6575ba636852867aebb686012ca36bf909826b
gyu <gyu@gyuui-MacBookPro.local>
2024-08-05 23:40:46 +0900

    Delete account_session expires

--- migrations/0004_create_account_session.sql
+++ migrations/0004_create_account_session.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS account_session (
     id VARCHAR(32) PRIMARY KEY,
-    account_id VARCHAR(42) UNIQUE NOT NULL
+    account_id VARCHAR(42) UNIQUE NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
 );
-
 
 
 
