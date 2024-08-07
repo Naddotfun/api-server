355f189e8ce1a2e88865e0b6817ec6cb557d6ca6
gyu <gyu@gyuui-MacBookPro.local>
2024-08-08 06:17:25 +0900

    fix error

--- src/server/routes/profile/mod.rs
+++ src/server/routes/profile/mod.rs
@@ -11,7 +11,7 @@ use path::ProfilePath;
 pub fn router() -> Router<AppState> {
     Router::new()
         .route(ProfilePath::Profile.as_str(), get(get_profile))
-        .route(ProfilePath::CoinCreated.as_str(), get(get_coins_held))
+        .route(ProfilePath::CoinCreated.as_str(), get(get_created_coins))
         .route(ProfilePath::CoinHeld.as_str(), get(get_coins_held))
         .route(ProfilePath::Replies.as_str(), get(get_replies))
         .route(ProfilePath::Followers.as_str(), get(get_followers))
