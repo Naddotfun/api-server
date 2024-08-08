ecee20bbbaee3a3ca801e828325317ca079d82c4
gyu <gyu@gyuui-MacBookPro.local>
2024-08-09 00:39:43 +0900

    fix error

--- src/server/routes/socket/subscribe.rs
+++ src/server/routes/socket/subscribe.rs
@@ -12,7 +12,7 @@ use tracing::info;
 use crate::db::postgres::controller::coinpage::CoinPageController;
 
 use crate::server::state::AppState;
-use crate::types::chart_type::ChartType;
+
 use crate::types::event::coin_message::CoinMessage;
 use crate::types::event::order::OrderMessage;
 use crate::types::event::order::OrderType;
